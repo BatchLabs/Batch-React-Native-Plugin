42863a6dabd51ab05f266443987d711acd6799b5
Arnaud Roland <arnaud.roland.ar@gmail.com>
2022-06-29 15:48:53 +0200

    scripts: update bump version script to handle changelog version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -2,6 +2,7 @@
 
 NEW_VERSION="$1"
 
-sed -i '' "s/\"version\": \"[0-9].[0-9].[0-9]\"/\"version\": \"$NEW_VERSION\"/" package.json
-sed -i '' "s/ReactNative\/[0-9].[0-9].[0-9]/\ReactNative\/$NEW_VERSION/" ./ios/RNBatch.h
-sed -i '' "s/ReactNative\/[0-9].[0-9].[0-9]/\ReactNative\/$NEW_VERSION/" ./android/src/main/java/com/batch/batch_rn/RNBatchModule.java
+sed -i '' "s/\"version\": \"[0-9].[0-9].[0-9]\"/\"version\": \"$NEW_VERSION\"/" ../package.json
+sed -i '' "s/ReactNative\/[0-9].[0-9].[0-9]/\ReactNative\/$NEW_VERSION/" ../ios/RNBatch.h
+sed -i '' "s/ReactNative\/[0-9].[0-9].[0-9]/\ReactNative\/$NEW_VERSION/" ../android/src/main/java/com/batch/batch_rn/RNBatchModule.java
+sed -i '' "s/UPCOMING/$NEW_VERSION/1" ../CHANGELOG.md
