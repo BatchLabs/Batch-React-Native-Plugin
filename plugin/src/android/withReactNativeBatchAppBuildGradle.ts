95bdf7b2925f0890962728d2df099866768d283b
Arnaud Roland <arnaud.roland.ar@gmail.com>
2023-12-21 15:53:56 +0100

    expo: fix do not disturb param (added a new one) [sc-67152]

--- plugin/src/android/withReactNativeBatchAppBuildGradle.ts
+++ plugin/src/android/withReactNativeBatchAppBuildGradle.ts
@@ -1,30 +1,24 @@
 import { ConfigPlugin, withAppBuildGradle } from '@expo/config-plugins';
-import { Props } from '../withReactNativeBatch';
-import { BATCH_SDK_VERISON, BATCH_DO_NOT_DISTURB_INITIAL_STATE } from '../constants';
 
+import { BATCH_SDK_VERISON, BATCH_DO_NOT_DISTURB_INITIAL_STATE } from '../constants';
+import { Props } from '../withReactNativeBatch';
 
 export const pushDependencies = (contents: string, props: Props): string => {
   let newContents = contents;
-  const doNotDistrub = props.enableDoNotDistrub || BATCH_DO_NOT_DISTURB_INITIAL_STATE;
-  const versionNameLine = newContents.match(
-    /versionName "([^"]*)"/
-  );
+  const doNotDisturb = props.enableDoNotDisturb || props.enableDoNotDistrub || BATCH_DO_NOT_DISTURB_INITIAL_STATE;
+  const versionNameLine = newContents.match(/versionName "([^"]*)"/);
 
   if (versionNameLine) {
-    const endOfVersionNameIndex = newContents.indexOf(
-      versionNameLine[0]
-    );
+    const endOfVersionNameIndex = newContents.indexOf(versionNameLine[0]);
 
     const start = newContents.substring(0, endOfVersionNameIndex);
-    const end = newContents.substring(
-      endOfVersionNameIndex + versionNameLine[0].length
-    );
+    const end = newContents.substring(endOfVersionNameIndex + versionNameLine[0].length);
     newContents =
-      start
-      + versionNameLine[0]
-      + `\n        resValue "string", "BATCH_API_KEY", "${props.androidApiKey}"`
-      + `\n        resValue "bool", "BATCH_DO_NOT_DISTURB_INITIAL_STATE", "${doNotDistrub}"`
-      + end;
+      start +
+      versionNameLine[0] +
+      `\n        resValue "string", "BATCH_API_KEY", "${props.androidApiKey}"` +
+      `\n        resValue "bool", "BATCH_DO_NOT_DISTURB_INITIAL_STATE", "${doNotDisturb}"` +
+      end;
   }
 
   const dependenciesString = 'dependencies {';
@@ -39,16 +33,13 @@ export const pushDependencies = (contents: string, props: Props): string => {
       start +
       `\n    implementation platform('com.google.firebase:firebase-bom:25.12.0')
     implementation "com.google.firebase:firebase-messaging"
-    api "com.batch.android:batch-sdk:${BATCH_SDK_VERISON}"`
-      + end;
+    api "com.batch.android:batch-sdk:${BATCH_SDK_VERISON}"` +
+      end;
   }
   return newContents;
 };
 
-export const withReactNativeBatchAppBuildGradle: ConfigPlugin<Props> = (
-  config,
-  props
-) => {
+export const withReactNativeBatchAppBuildGradle: ConfigPlugin<Props> = (config, props) => {
   return withAppBuildGradle(config, async conf => {
     const content = conf.modResults.contents;
     const newContents = pushDependencies(content, props);
