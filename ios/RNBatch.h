35e45078aa7c76e9f01f01dec86b98ab141194b9
Arnaud Roland <arnaud.roland.ar@gmail.com>
2024-01-08 16:26:37 +0100

    all: update batch to 1.21

--- ios/RNBatch.h
+++ ios/RNBatch.h
@@ -1,10 +1,10 @@
-#if __has_include("React/RCTBridgeModule.h")
+#if __has_include(<React/RCTBridgeModule.h>)
   #import <React/RCTBridgeModule.h>
 #else
   #import "RCTBridgeModule.h"
 #endif
 
-#if __has_include("React/RCTEventEmitter.h")
+#if __has_include(<React/RCTEventEmitter.h>)
   #import <React/RCTEventEmitter.h>
 #else
   #import "RCTEventEmitter.h"
