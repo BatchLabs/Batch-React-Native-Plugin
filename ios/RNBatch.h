c2882b73fe3d9b986d73dad258377a8e392e0f5f
Arnaud Roland <arnaud.roland@batch.com>
2024-12-13 16:04:23 +0100

    all: bump version to 10.0.1

--- ios/RNBatch.h
+++ ios/RNBatch.h
@@ -1,7 +1,7 @@
 #import <React/RCTEventEmitter.h>
 #import <Batch/Batch.h>
 
-#define PluginVersion "ReactNative/10.0.0"
+#define PluginVersion "ReactNative/10.0.1"
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import <RNBatchSpec/RNBatchSpec.h>
