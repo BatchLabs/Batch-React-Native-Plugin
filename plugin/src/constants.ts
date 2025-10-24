246de63315eb44c8832fd1a4727a95d336003db4
Arnaud Roland <arnaud.roland@batch.com>
2025-10-24 14:24:51 +0200

    all: remove expo support
    # Conflicts:
    #	plugin/src/__tests__/withReactNativeBatchMainActivity.test.ts
    #	plugin/src/android/withReactNativeBatchMainActivity.ts
    #	plugin/src/fixtures/mainActivity.ts
    #	plugin/src/withReactNativeBatch.ts

--- plugin/src/constants.ts
+++
@@ -1,6 +0,0 @@
-//Define the version used in the pre-built gradle file for the batch native sdk dependency.
-export const BATCH_SDK_VERSION = '3.1.+';
-export const BATCH_DO_NOT_DISTURB_INITIAL_STATE = false;
-export const BATCH_DEFAULT_OPT_OUT_INITIAL_STATE = false;
-export const BATCH_DEFAULT_PROFILE_CUSTOM_ID_MIGRATION = true;
-export const BATCH_DEFAULT_PROFILE_CUSTOM_DATA_MIGRATION = true;
--- plugin/src/ios/withReactNativeBatchEntitlements.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigPlugin, withEntitlementsPlist } from '@expo/config-plugins';
-
-export const withReactNativeBatchEntitlements: ConfigPlugin<object | void> = config => {
-  return withEntitlementsPlist(config, config => {
-    config.modResults = { ...config.modResults, 'aps-environment': 'development' };
-    return config;
-  });
-};
